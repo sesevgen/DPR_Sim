66fa939f1fc8b8d00f2e64cfe834f8c9332d1806
sesevgen <sesevgen@gmail.com>
2019-05-14 22:43:10 -0500

    Basic Flask setup

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from utils import Diceset, d20Set, Action, Statistics
 import matplotlib.pyplot as plt
 
 
+from flask import Flask
 
-d20 = d20Set(1)
-diceset = Diceset([(8,6)])
-action = Action(d20,10,diceset,crit_numbers=[],fail_dmg_scale=0.5)  
-stats = Statistics(action)
-stats.collect_statistics()
+app = Flask(__name__)
 
-stats.plot_histogram()
-plt.savefig('plot')
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+#d20 = d20Set(1)
+#diceset = Diceset([(8,6)])
+#action = Action(d20,10,diceset,crit_numbers=[],fail_dmg_scale=0.5)  
+#stats = Statistics(action)
+#stats.collect_statistics()
+
+#stats.plot_histogram()
+#plt.savefig('plot')
 
 
