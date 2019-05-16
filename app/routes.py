0a65f5d16c502ca507dde9752d73523d319d5ef4
sesevgen <sesevgen@gmail.com>
2019-05-16 01:08:46 -0500

    Fix matplotlib save bug

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,8 @@ import os
 
 import matplotlib.pyplot as plt
 
-app = Flask(__name__, static_url_path='/static')      
- 
+app = Flask(__name__, static_url_path='/static')
+
 @app.route('/')
 def home():
     dice1 = 8
@@ -20,13 +20,13 @@ def home():
     stats.collect_statistics()
 
     return render_template('home.html') #, imagepath='static/img/placeholder.png', stats=stats.report_statistics())
-    
-  
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/distributions')    
+@app.route('/distributions')
 def distributions():
     dice1 = 8
     dice2 = 6
@@ -34,9 +34,9 @@ def distributions():
     reroll_lowest = 0
     roll_min = 0
     drop_lowest = 0
-    
+
     details = [dice1,dice2,reroll_equal_to,reroll_lowest,roll_min,drop_lowest]
-    
+
     d20 = d20Set(1)
     diceset= Diceset([(dice1,dice2)])
     action = Action(d20,0,diceset,crit_numbers=[],fail_dmg_scale=0.5)
@@ -44,15 +44,16 @@ def distributions():
     stats.collect_statistics()
 
     return render_template('distributions.html', imagepath='static/img/placeholder.png', stats=stats.report_statistics(), details=details)
-    
-    
+
+
 @app.route('/calculate',methods=['POST'])
 def calculate():
-    dir = 'static/img/temp/'
+    root = os.path.dirname(__file__)
+    dir = os.path.join(root,'static/img/temp/')
     files = os.listdir(dir)
     for file in files:
         os.remove(os.path.join(dir,file))
-        
+
     dice1 = int(request.form.get('dice1'))
     dice2 = int(request.form.get('dice2'))
     try:
@@ -73,26 +74,26 @@ def calculate():
         drop_lowest = int(request.form.get('drop_lowest'))
     except:
         drop_lowest = 0
-    
+
     details = [dice1,dice2,reroll_equal_to,reroll_lowest,roll_min,drop_lowest]
-    
+
     d20 = d20Set(1)
     diceset= Diceset([(dice1,dice2)],reroll_equal_to,roll_min,reroll_lowest,drop_lowest)
     action = Action(d20,0,diceset,crit_numbers=[],fail_dmg_scale=0.0)
     stats = Statistics(action)
     stats.collect_statistics()
     stats.plot_histogram()
-    
+
     plotname = 'static/img/temp/'
     for detail in details:
         plotname = plotname + str(detail)
     plotname = plotname + '.png'
 
-    plt.savefig(plotname)
+    plt.savefig(os.path.join(root,plotname))
     plt.clf()
 
     return render_template('distributions.html', imagepath=plotname, stats=stats.report_statistics(),details=details)
-    
-    
+
+
 if __name__ == '__main__':
   app.run(debug=True)
